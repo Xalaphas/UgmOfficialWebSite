14fba0a922ea5cb4b4fd15673fffdddc08a38a25
Xalphas Dev <raphaelrdgss.006@gmail.com>
2024-09-21 23:38:09 +0000

    Changing text logic

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 function changeText() {
-    document.getElementById("myText").innerHTML = "SOCORRO NÂO AGUENTO MAIS FAZER PROGRMAAAAA";
+    const text = document.getElementById('myText');
+
+    if (text.innerHTML == "Clique aqui por favor") {
+        text.innerHTML = "SOCORROOOOOO ALGUÈM ME AJUDAA NÂO AGUENTO MAIS FAZER PROGRAMAAAAAAAA";
+    } else {
+        text.innerHTML = "Clique aqui por favor";
+    }
 }
